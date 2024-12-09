cbf0e7818475a76ff0fb84fc7f149ceaba9472b3
Sahil Kumar <kumarsahil7753@gmail.com>
2024-12-09 21:08:27 +0530

    changed link

--- client/src/components/CompleteService.jsx
+++ client/src/components/CompleteService.jsx
@@ -8,7 +8,7 @@ const CompleteService = ({ service }) => {
   const handleCompleteService = async () => {
     setIsProcessing(true);
     try {
-      const response = await axios.post('https://localtimebankingplatform-m2jh.onrender.com:3001/api/transactions', {
+      const response = await axios.post('https://localtimebankingplatform-m2jh.onrender.com/api/transactions', {
         serviceId: service._id,
         toUserId: service.userId,
         credits: service.credits,
